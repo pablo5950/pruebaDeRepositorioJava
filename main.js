06629a10da1b87ea06df95a84afa40117f315f1e
pablo5950 <pgambero1@gmail.com>
2025-05-13 18:29:19 -0300

    se sumaron los atributos async y defer

---
+++ main.js
@@ -0,0 +1,21 @@
+//VARIABLE: es un espacio de memoria reservado para almacenar informacion importante del programa.
+
+// 1-DECLARACION:
+let cliente;
+
+
+// 2-ASIGNACIÓN DE VALORES:
+cliente = "Carlos";
+cliente = "Samuel"; //pisó carlos y se quedó con el ultimo nombre que es samuel
+console.log(cliente);
+
+
+// 3-INICIALIZAR VARIABLES:
+ let edad = 37;
+ console.log(edad);
+
+
+ //CONSTANTE: es una variable cuyo valor no puede ser modificado una vez que ha sido asignado,por ejemplo:fecha de nac.
+
+ const nacimiento = 1988;
+ console.log(nacimiento);
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("el script está funcionando correctamente")
+
+const titulo = document.getElementById("titulo");
+titulo.textContent = "texto modificado"
