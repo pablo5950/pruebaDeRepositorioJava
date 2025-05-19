7433bfe79697832d4032e3ee837387f6811db58a
pablo5950 <pgambero1@gmail.com>
2025-05-19 12:52:32 -0300

    Inicio del respositorio con el ejemplo de las unidad 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// demo operadores:
+
+let edad =  18;
+let dinero = true;
+
+if( edad >= 18 && dinero) { // operador logico and (&&) = se tienen q cumplir ambas condiciones para q el resultado sea true
+ console.log("podes ingresar al bar");
+} else {
+ console.log("sos menor,anda a estudiar piscui");
+}
+
+
